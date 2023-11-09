3d93bbb5ad63a03ec526699825da036c79b3d257
Samuel Thng <zthng@ap.equinix.com>
2023-11-09 19:06:05 +0800

    Updated: Swap A and B rendering position

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ComponentA />
         <ComponentB />
+        <ComponentA />
       </header>
     </div>
   );

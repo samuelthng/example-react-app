11d897539c38ab113ef996efb68a4ae949f8f6c6
Samuel Thng <zthng@ap.equinix.com>
2023-11-09 18:58:16 +0800

    Added: Component A and B Text
    Added: Component B Text
    
    Updated: Component A Text

--- src/component/ComponentA.js
+++ src/component/ComponentA.js
@@ -2,6 +2,7 @@ export default function ComponentA() {
   return (
     <div>
       <h1>Component A</h1>
+      <p>Update Component A Text</p>
     </div>
   );
 }
--- src/component/ComponentB.js
+++ src/component/ComponentB.js
@@ -2,6 +2,7 @@ export default function ComponentB() {
   return (
     <div>
       <h1>Component B</h1>
+      <p>This is some text for Component B</p>
     </div>
   );
 }
